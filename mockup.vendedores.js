ef1986fe550a2fbf4891982edda06a492971f206
A𝜗ৎ <anaalvssss.6@gmail.com>
2026-08-18 11:47:42 -0300

    Update mockup.vendedores.js

--- mockup.vendedores.js
+++ mockup.vendedores.js
@@ -3,23 +3,25 @@ let funcionarios = [
 {   "nome":"Beatriz Alves",
     "matricula":"1234",
     "salario":2400,
-    "comissao":0.5
+    "comissao":0.15
 },
 {   "nome":"Sara de Paula",
     "matricula":"2456",
     "salario":2400,
-    "comissao":0.6
+    "comissao":0.10
 },
 {   "nome":"Vitória Bueno",
     "matricula":"3678",
     "salario":2250,
-    "comissao":0.4
+    "comissao":0.10
 },
 {   "nome":"Letícia Souza",
     "matricula":"9807",
     "salario":2000,
-    "comissao":0.5
+    "comissao":0.10
 },
 ];
+
+module.exports = funcionarios;
 
 module.exports = funcionarios;
